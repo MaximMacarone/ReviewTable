4470f06e7b1e18e24dad9b9f14a285e5de3215b7
Maxim Makarenkov <maximmacarone@gmail.com>
2025-06-28 12:54:39 +0300

    Initial Commit

---
+++ ReviewTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReviewTable
+//
+//  Created by Maxim Makarenkov on 28.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
